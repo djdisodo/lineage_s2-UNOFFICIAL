a2ab38158cd3efdbf14ccdaed7acbd7bf8f90a0d
djdisodo <djdisodo@gmail.com>
2019-10-28 22:47:15 +0900

    fix

--- travis.sh
+++ travis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git config --global color.ui false
 repo init -u https://github.com/LineageOS/android.git -b lineage-17.0
 mkdir .repo/local_manifests
 mv roomservice.xml .repo/local_manifests/
