3f8b90f8673af1c89db5a63c2f6838a792c14055
djdisodo <djdisodo@gmail.com>
2019-10-28 22:57:39 +0900

    quiet

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ git config --global color.ui false
 repo init -u https://github.com/LineageOS/android.git -b lineage-17.0
 mkdir .repo/local_manifests
 mv roomservice.xml .repo/local_manifests/
-repo sync -j32
+repo sync -j32 -q -c
 source build/envsetup.sh
 export USE_CCACHE=1
 ccache -M 50.0G
